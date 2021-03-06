81eefc4e44aafc622a2868220b06d772b2978b3c
Asane <yaradayo317@gmail.com>
2021-03-06 20:37:09 +0900

    added useState

--- src/components/alert/alert.component.jsx
+++ src/components/alert/alert.component.jsx
@@ -20,6 +20,10 @@ const useStyles = makeStyles((theme) => ({
 const ActionAlerts = ({ message, severity }) => {
   const classes = useStyles();
 
+  const [opendialog, setOpendialog] = React.useState(true);
+  const handleOpen = () => {
+    setOpendialog(true);
+  };
   const handleClose = () => {
     setOpendialog(false);
     console.log(opendialog);
@@ -41,3 +45,13 @@ const ActionAlerts = ({ message, severity }) => {
 };
 
 export default ActionAlerts;
+
+//   <Alert
+//     severity={props.severity}
+//     onClose={() => {
+//       handleClose();
+//     }}
+//     className={`${opendialog ? "openAlert" : classes.closeAlert} `}
+//   >
+//     {props.message}
+//   </Alert>;
